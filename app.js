28ad17a8d09f4a2422eb64aaad28d00180fcab16
Paul Stevens <twinkisan@hotmail.com>
2020-10-15 16:46:50 -0700

    wireframe question styling

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ import { renderUserInput } from '../utils/render-user-input.js';
 
 // set event listeners to update state and DOM
 
-let searchParams = new URLSearchParams(window.location.search);
-// console.log(searchParams.get('id'));
-const mainSection = document.getElementById('main-section');
-const userObject = getUserLocalStorage(searchParams.get('id'));
-mainSection.innerHTML = '';
+// let searchParams = new URLSearchParams(window.location.search);
+// // console.log(searchParams.get('id'));
+// const mainSection = document.getElementById('main-section');
+// const userObject = getUserLocalStorage(searchParams.get('id'));
+// mainSection.innerHTML = '';
 
-if (!userObject) {
-    let element = renderUserInput();
-    mainSection.appendChild(element);
+// if (!userObject) {
+//     let element = renderUserInput();
+//     mainSection.appendChild(element);
 
-} else {
+// } else {
     
 
-    }
+//     }
 
 
