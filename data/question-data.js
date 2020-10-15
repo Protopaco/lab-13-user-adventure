f123fe52346f6343889030581084f47a3aaf0bca
Paul Stevens <twinkisan@hotmail.com>
2020-10-15 16:30:48 -0700

    added question-data JSON

---
+++ data/question-data.js
@@ -0,0 +1,78 @@
+
+export default {
+    friends: {
+        id: 'friends',
+        preview: {
+            text: 'with friends',
+            img_src: '../assets/friends-thumb.jpg'
+                },
+        frame: {
+            text: 'How do you hide your pain from your friends?',
+            img_src: '../assets/friends-frame.jpg',
+        },
+        questions: {   
+            inward: {
+                text: 'Ignoring them',
+                img_src: '../assets/friends-inward.jpg',
+            },
+            shutdown: {
+                text: 'Leaving early',
+                img_src: '../assets/friends-shutdown.jpg',
+            },
+            outward: {
+                text: 'Talking trash',
+                img_src: '../assets/friends-outward.jpg'
+            }
+                }
+        },
+    work: {
+        id: 'work',
+        preview: {
+            text: 'at work',
+            img_src: '../assets/work-thumb.jpg'
+                },
+        frame: {
+            text: 'How do you hide your pain at work?',
+            img_src: '../assets/work-frame.jpg',
+        },
+        questions: {   
+            inward: {
+                text: 'Drinking',
+                img_src: '../assets/work-inward.jpg',
+            },
+            shutdown: {
+                text: 'Inappropriate naps',
+                img_src: '../assets/work-shutdown.jpg',
+            },
+            outward: {
+                text: 'Lashing out',
+                img_src: '../assets/work-outward.jpg'
+            }
+                }
+    },
+    alone: {
+        id: 'alone',
+        preview: {
+            text: 'alone',
+            img_src: '../assets/alone-thumb.jpg'
+                },
+        frame: {
+            text: "How do you hide your pain when you're alone?",
+            img_src: '../assets/alone-frame.jpg',
+        },
+        questions: {   
+            inward: {
+                text: 'Overeating',
+                img_src: '../assets/alone-inward.jpg',
+            },
+            shutdown: {
+                text: 'Drugs',
+                img_src: '../assets/alone-shutdown.jpg',
+            },
+            outward: {
+                text: 'Internet trolling',
+                img_src: '../assets/alone-outward.jpg'
+            }
+                }
+    }
+}
