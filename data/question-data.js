70f6e6217b8fb14e55496fe6a5955a9de79d3120
Paul Stevens <twinkisan@hotmail.com>
2020-10-16 08:50:55 -0700

    fixed photo pathing issue

--- data/question-data.js
+++ data/question-data.js
@@ -4,27 +4,27 @@ export default [
         id: 'friends',
         preview: {
             text: 'with friends',
-            img_src: '../assets/friends-thumb.jpg'
+            img_src: './assets/friends-thumb.jpg'
                 },
         frame: {
             text: 'how do you hide your pain from your friends?',
-            img_src: '../assets/friends-frame.jpg',
+            img_src: './assets/friends-frame.jpg',
         },
         questions: {   
             inward: {
                 id: 'inward',
                 text: 'Ignoring them',
-                img_src: '../assets/friends-inward.jpg',
+                img_src: './assets/friends-inward.jpg',
             },
             shutdown: {
                 id: 'shutdown',
                 text: 'Leaving early',
-                img_src: '../assets/friends-shutdown.jpg',
+                img_src: './assets/friends-shutdown.jpg',
             },
             outward: {
                 id: 'outward',
                 text: 'Talking trash',
-                img_src: '../assets/friends-outward.jpg'
+                img_src: './assets/friends-outward.jpg'
             }
                 }
         },
@@ -32,27 +32,27 @@ export default [
         id: 'work',
         preview: {
             text: 'at work',
-            img_src: '../assets/work-thumb.jpg'
+            img_src: './assets/work-thumb.jpg'
                 },
         frame: {
             text: 'how do you hide your pain at work?',
-            img_src: '../assets/work-frame.jpg',
+            img_src: './assets/work-frame.jpg',
         },
         questions: {   
             inward: {
                 id: 'inward',
                 text: 'Drinking',
-                img_src: '../assets/work-inward.jpg',
+                img_src: './assets/work-inward.jpg',
             },
             shutdown: {
                 id: 'shutdown',
                 text: 'Inappropriate naps',
-                img_src: '../assets/work-shutdown.jpg',
+                img_src: './assets/work-shutdown.jpg',
             },
             outward: {
                 id: 'outward',
                 text: 'Lashing out',
-                img_src: '../assets/work-outward.jpg'
+                img_src: './assets/work-outward.jpg'
             }
                 }
     },
@@ -60,27 +60,27 @@ export default [
         id: 'alone',
         preview: {
             text: 'alone',
-            img_src: '../assets/alone-thumb.jpg'
+            img_src: './assets/alone-thumb.jpg'
                 },
         frame: {
             text: "how do you hide your pain when you're alone?",
-            img_src: '../assets/alone-frame.jpg',
+            img_src: './assets/alone-frame.jpg',
         },
         questions: {   
             inward: {
                 id: 'inward',
                 text: 'Overeating',
-                img_src: '../assets/alone-inward.jpg',
+                img_src: './assets/alone-inward.jpg',
             },
             shutdown: {
                 id: 'shutdown',
                 text: 'Drugs',
-                img_src: '../assets/alone-shutdown.jpg',
+                img_src: './assets/alone-shutdown.jpg',
             },
             outward: {
                 id: 'outward',
                 text: 'Internet trolling',
-                img_src: '../assets/alone-outward.jpg'
+                img_src: './assets/alone-outward.jpg'
             }
                 }
     }
